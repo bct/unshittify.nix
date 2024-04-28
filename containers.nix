6ca88abb8d541f65f859963b25af4c0012d73c4e
Brendan Taylor <bct@diffeq.com>
2024-04-27 20:30:12 -0600

    the container boots miniflux

--- containers.nix
+++ containers.nix
@@ -14,7 +14,21 @@
         configuration = { pkgs, lib, ... }: {
           # Network configuration.
           networking.useDHCP = false;
-          networking.firewall.allowedTCPPorts = [ 8080 ];
+          networking.firewall.allowedTCPPorts = [ 8080 8081 ];
+
+          services.miniflux = {
+            enable = true;
+            package = withSystem pkgs.stdenv.hostPlatform.system ({ config, ... }:
+              config.packages.miniflux
+            );
+            config = {
+              LISTEN_ADDR = "0.0.0.0:8081";
+            };
+            adminCredentialsFile = pkgs.writeText "miniflux-admin-credentials.env" ''
+              ADMIN_USERNAME=admin
+              ADMIN_PASSWORD=miniflux
+            '';
+          };
 
           services.nitter = {
             enable = true;
